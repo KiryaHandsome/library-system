2c0cc00bc79b81b65190843e43a475059792d0bd
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-10-08 18:11:44 +0300

    refactor: add check before running requests

--- book-service/src/main/java/com/modsen/book/BookServiceApplication.java
+++ book-service/src/main/java/com/modsen/book/BookServiceApplication.java
@@ -1,6 +1,8 @@
 package com.modsen.book;
 
 import com.modsen.book.dto.BookCreate;
+import com.modsen.book.model.Book;
+import com.modsen.book.repository.BookRepository;
 import com.modsen.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +11,8 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,6 +25,7 @@ public class BookServiceApplication implements ApplicationRunner {
 
     @Value("${book.url}")
     private String bookServiceUrl;
+    private final BookRepository bookRepository;
     private final RestTemplate restTemplate;
 
     public static void main(String[] args) {
@@ -29,12 +34,16 @@ public class BookServiceApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        List<BookCreate> books = JsonUtil.getBooks();
-        for (BookCreate book : books) {
-            try {
-                HttpEntity<BookCreate> requestEntity = new HttpEntity<>(book);
-                restTemplate.postForEntity(bookServiceUrl, requestEntity, String.class);
-            } catch (Exception ignored) {}
+        Page<Book> all = bookRepository.findAll(Pageable.ofSize(1));
+        if (all.isEmpty()) { // if db is empty create books
+            List<BookCreate> books = JsonUtil.getBooks();
+            for (BookCreate book : books) {
+                try {
+                    HttpEntity<BookCreate> requestEntity = new HttpEntity<>(book);
+                    restTemplate.postForEntity(bookServiceUrl, requestEntity, String.class);
+                } catch (Exception ignored) {
+                }
+            }
         }
     }
 }
